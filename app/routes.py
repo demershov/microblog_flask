7c8fec60618da23683eb66d729420ab1f0ca3279
Dmitry Ershov <degalweb@icloud.com>
2018-01-25 04:49:28 +0300

    Создан шаблон

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,23 @@
 from app import app
+from flask import render_template
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World'
+    user = {'username': 'Григорий'}
+    posts = [
+        {
+            'author': 'Михаил',
+            'body': 'Мой первый блог на Фласке'
+        },
+        {
+            'author': 'Дмитрий',
+            'body': 'Раз, два, три, кто не спрятался, я не виноват'
+        },
+        {
+            'author': 'Александр',
+            'body': 'Кто тум самый храбрый?'
+        }
+    ]
+    return render_template('index.html', title='Микроблог', user=user)
