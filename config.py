fa7617179420e222f636b3f24bf4218717bcae83
Dmitry Ershov <degalweb@icloud.com>
2018-01-27 17:36:43 +0300

    Создана база данных
    Произведена миграция моделей юзера и постов

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'f2gh3n3s9dfs5nj4fsu32js2hf3sj1'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
